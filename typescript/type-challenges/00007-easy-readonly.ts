d4fc98d9219987d96164b1f1e47519947327524c
panjh <53244269+3000-codes@users.noreply.github.com>
2023-03-28 10:03:45 +0800

    start medium

--- typescript/type-challenges/00007-easy-readonly.ts
+++ typescript/type-challenges/00007-easy-readonly.ts
@@ -1,16 +1,21 @@
-interface Todo {
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type cases = [
+  Expect<Equal<MyReadonly<Todo1>, Readonly<Todo1>>>,
+]
+
+interface Todo1 {
   title: string
   description: string
+  completed: boolean
+  meta: {
+    author: string
+  }
 }
 
-const todo: MyReadonly<Todo> = {
-  title: "Hey",
-  description: "foobar"
-}
 
-todo.title = "Hello" // Error: cannot reassign a readonly property
-todo.description = "barFoo" // Error: cannot reassign a readonly property
-
+// ============= Your Code Here =============
 type MyReadonly<T>={
   readonly [K in keyof T]:T[K]
 }
--- typescript/type-challenges/00014-easy-first.ts
+++ typescript/type-challenges/00014-easy-first.ts
@@ -1,8 +1,20 @@
-type arr1 = ['a', 'b', 'c']
-type arr2 = [3, 2, 1]
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
 
-type head1 = First<arr1> // expected to be 'a'
-type head2 = First<arr2> // expected to be 3
+type cases = [
+  Expect<Equal<First<[3, 2, 1]>, 3>>,
+  Expect<Equal<First<[() => 123, { a: string }]>, () => 123>>,
+  Expect<Equal<First<[]>, never>>,
+  Expect<Equal<First<[undefined]>, undefined>>,
+]
 
+type errors = [
+  // @ts-expect-error
+  First<'notArray'>,
+  // @ts-expect-error
+  First<{ 0: 'arrayLike' }>,
+]
 
-type First<T extends any[]>=T[0]
+// ============= Your Code Here =============
+
+type First<T extends any[]>=T['length'] extends 0 ?never :T[0]
