63fb6dc16e617a322d71ed0137e2a1d43b30f318
EthanPan6 <53244269+3000-codes@users.noreply.github.com>
2023-04-02 20:57:53 +0800

    replace(all)

---
+++ typescript/type-challenges/00020-medium-promise-all.ts
@@ -0,0 +1,26 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+const promiseAllTest1 = PromiseAll([1, 2, 3] as const)
+const promiseAllTest2 = PromiseAll([1, 2, Promise.resolve(3)] as const)
+const promiseAllTest3 = PromiseAll([1, 2, Promise.resolve(3)])
+const promiseAllTest4 = PromiseAll<Array<number | Promise<number>>>([1, 2, 3])
+
+type cases = [
+  Expect<Equal<typeof promiseAllTest1, Promise<[1, 2, 3]>>>,
+  Expect<Equal<typeof promiseAllTest2, Promise<[1, 2, number]>>>,
+  Expect<Equal<typeof promiseAllTest3, Promise<[number, number, number]>>>,
+  Expect<Equal<typeof promiseAllTest4, Promise<number[]>>>,
+]
+
+
+// ============= Your Code Here =============
+// declare function PromiseAll<T extends unknown[]>(values: readonly [...T]):Promise <{
+//   [Index in keyof T]:T[Index] extends PromiseLike<infer R>?R:T[Index]
+// }>
+
+// type MyAwaited<T> = T extends PromiseLike<infer U> ? U : T
+
+declare function PromiseAll<T extends unknown[]>(values: readonly [...T]): Promise<{
+  [Key in keyof T]: Awaited<T[Key]>
+}>
