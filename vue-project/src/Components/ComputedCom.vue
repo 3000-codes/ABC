ecbfef5258ed295cd03924dcaf724a29290c329a
panjh <53244269+3000-codes@users.noreply.github.com>
2023-08-28 10:09:22 +0800

    add: .

---
+++ vue-project/src/Components/ComputedCom.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+const num = ref(1)
+const num2 = num.value + 1 // num2 will not be reactive
+const num3 = computed(() => num.value + 1)
+const increment = () => {
+  num.value++
+}
+</script>
+
+<template>
+  <h2>num:{{ num }}</h2>
+  <h2>num2:{{ num2 }}</h2>
+  <h2>num3:{{ num3 }}</h2>
+  <button @click="increment">increment</button>
+</template>
--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,28 +1,21 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
   <main>
-    <div>
-      Lorem ipsum dolor sit amet consectetur adipi
-
-    </div>
+    <div>Lorem ipsum dolor sit amet consectetur adipi</div>
   </main>
 </template>
 <style lang="scss" scoped>
-main{
+main {
   width: 100%;
   height: 100vh;
   display: flex;
 
-  div{
+  div {
     width: 100%;
     height: 100%;
     font-size: 70px;
     font-weight: bolder;
     position: relative;
-    &::before{
+    &::before {
       content: 'Lorem ipsum dolor sit amet consectetur adipi';
       position: absolute;
       top: 0;
@@ -33,18 +26,17 @@ main{
       color: #06ee99;
       border-right: 2px solid #06ee99;
       animation: animate 3s linear infinite;
-      filter:drop-shadow(0 0 10px #06ee99);
+      filter: drop-shadow(0 0 10px #06ee99);
     }
 
-    @keyframes animate{
-      0%{
+    @keyframes animate {
+      0% {
         width: 0;
       }
-      100%{
+      100% {
         width: 100%;
       }
     }
   }
 }
-
 </style>
